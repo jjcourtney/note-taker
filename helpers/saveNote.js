aad4423bff03dce8739dd58a00e2f9edb340c367
jjcourtney <jcourtneywork@gmail.com>
2021-08-21 01:23:59 +0100

    bug: tested to work

--- helpers/saveNote.js
+++ helpers/saveNote.js
@@ -1,10 +1,9 @@
 const path = require('path');
 
 const saveNote = (req, res) => {
-    //console.log("save res", res);
+   
     console.log("save req", req.body);
-    res.json("nice one")
-
+    res.json("Save request successful")
 }
 
 module.exports = saveNote;
