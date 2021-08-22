2009e1352cb5261b73715b5ef04b9f3567edb4ab
jjcourtney <jcourtneywork@gmail.com>
2021-08-22 16:34:22 +0100

    canged case of PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const api = require('./routes/api');
 const path = require('path');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
