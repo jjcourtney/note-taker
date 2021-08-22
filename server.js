352ea02b18bf060b84e851c04420e989c0f0e513
jjcourtney <jcourtneywork@gmail.com>
2021-08-22 22:04:57 +0100

    feat: added wildcard route

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) =>{
   res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
