16f36a547cc293f562600e003f36b4bd42b0a367
jjcourtney <jcourtneywork@gmail.com>
2021-08-22 15:37:35 +0100

    bug: routing for webpage handled by server.js

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 
 const api = require('./routes/api');
-const homepage = require('./routes/index');
+const path = require('path');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
+
+app.use(express.static('public'));
 
 // Middleware being parsed
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', api);
-app.use(homepage)
+
+
+
+app.get('/notes', (req, res) =>{
+  console.log("/notes hit")
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+
 
 
 app.listen(PORT, () =>
