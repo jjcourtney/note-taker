f4a1b65c63d2308b2255fd830459710c8075d9b1
jjcourtney <jcourtneywork@gmail.com>
2021-08-22 14:54:54 +0100

    feat: added fuctionality to save to JSON

--- helpers/saveNote.js
+++ helpers/saveNote.js
@@ -1,9 +1,28 @@
-const path = require('path');
+
+const fs = require('fs');
+
+
+const addNoteToJson = (file, noteContent) =>
+  fs.writeFile(file, JSON.stringify(noteContent, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${file}`)
+  );
 
 const saveNote = (req, res) => {
-   
+    const noteContent = req.body;
+    const file = "./db/db.json";
+
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedData = JSON.parse(data);
+          parsedData.push(noteContent);
+          addNoteToJson(file, parsedData);
+        }
+      });
+
     console.log("save req", req.body);
-    res.json("Save request successful")
+    res.status(418).send("Wrote to file");
 }
 
 module.exports = saveNote;
