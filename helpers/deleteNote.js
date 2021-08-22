6035dc464a44064d9f8abd433070c9b331ee9f8c
jjcourtney <jcourtneywork@gmail.com>
2021-08-22 15:10:30 +0100

    feat: added fuction to delete entry

--- helpers/deleteNote.js
+++ helpers/deleteNote.js
@@ -1,7 +1,21 @@
-const path = require('path');
 
-const deleteNote = id => {
-console.log(id)
+const { readFromJson, addNoteToJson } = require('./fsUtils')
+
+const deleteNote = title => {
+
+    const file = './db/db.json';
+    readFromJson(file)
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      
+      const result = json.filter((note) => note.title !== title);
+
+
+      addNoteToJson(file, result);
+
+
+      res.json(`Notes with the title ${title} have been deleted 🗑️`);
+    });
 
 }
 
