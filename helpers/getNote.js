3b7da88b8f8188bc6c00aca5ea082113c0513cc3
jjcourtney <jcourtneywork@gmail.com>
2021-08-22 14:54:35 +0100

    Feat: added fuctionality to return values frm JSON

--- helpers/getNote.js
+++ helpers/getNote.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const fs = require('fs');
+const util = require('util')
+
+
+const readFromFile = util.promisify(fs.readFile);
 
 const getNote = (req, res)=> {
 
-    res.json("Get request working")
+    readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)));
 
 }
 
